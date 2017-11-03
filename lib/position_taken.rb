a2bc8d05bd7d216b6c6b888056c2316aeff28cf1
Crista Cooper <aine3d@gmail.com>
2017-11-03 19:35:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || ""
-    return false
-  else board[index] == " X " || board[index] == " O "
+  if board[index] == " X " || board[index] == " O "
     return true
+  else board[index] == " " || ""
+  return false
   end
 end
